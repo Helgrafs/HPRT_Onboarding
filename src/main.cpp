3f60e4a685f109e25186fc25d15c2796cb1e89c2
Helgrafs <mgrinums@gmail.com>
2023-06-14 22:22:33 +0300

    very minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <Arduino.h>
 #include <Adafruit_BMP280.h>
+
+//Defining pins used, side note, gotta remember to wire it up exactly like this
+#define BMP_SCK (12)
+#define BMP_MISO (11)
+#define BMP_MOSI (10)
+
+Adafruit_BMP280 bmp; //links the driver for the barometer to that variable
 // put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  int result = myFunction(2, 3);
-}
+  while ( !Serial ) delay(100);
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  status = bmp.begin(); //this actually initializes sensor
+
+  }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
