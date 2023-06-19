f016daaae8576cfb3b3269779c2914f4cde88c40
Helgrafs <mgrinums@gmail.com>
2023-06-19 15:17:50 +0300

    Done, just a glitch left

--- src/main.cpp
+++ src/main.cpp
@@ -3,61 +3,63 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_BMP280.h>
+//LoRa definitions, shamelessly stolen from VIP code, after cross referencing to datasheet, I get it now.
+#define SS 18 
+#define RST 14
+#define DI0 26
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+Adafruit_BMP280 bmp;   
 
-Adafruit_BMP280 bmp;
+String outgoing = "string";
+int syncWord = 0xF7;
+byte _localAddress = 0xFF;
+byte _destination = 0xFF;
+boolean _canPrintHeaderMessage = false;
 void setup() {
   // VERY IMPORTANT!!!! WHEN CONNECTING BMP280, SCL goes to pin 11 And SDA goes to pin 12!!!!!!!!!!!
   Serial.begin(115200);
   while (!Serial);
   unsigned status;
   status = bmp.begin();
-  // if (!status) {
-  //   Serial.println(F("BMP Initialisation failed"));
-  //       while (1) delay(10);
-  // } else {
-  //   Serial.println("BMP initialisation successful");
-  // }
+  SPI.begin(SCK, MISO, MOSI, SS);
+  LoRa.setPins(SS, RST, DI0);
+    if (!status) {
+      Serial.println(F("BMP Initialisation failed"));
+          while (1) delay(10);
+    } else {
+      Serial.println("BMP initialisation successful");
+    }
   Serial.println("LoRa Transmitter");
-LoRa.begin(433E6);
-//  if (!LoRa.begin(433E6)) {
-//    Serial.println("Starting LoRa failed!");
-//    while (1);
-//   }else {
-//    Serial.println("LoRa initialisation successful");
-//  }
-  
-  
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+   }else {
+    Serial.println("LoRa initialisation successful");
+ }
+  LoRa.setSyncWord(0xF7);
+  LoRa.setTxPower(20);
+  LoRa.setSpreadingFactor(9);
+  LoRa.setCodingRate4(7);
+  LoRa.setSignalBandwidth(31.25E3);
 }
-void Barometer() {
-       Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1017)); // as close as i could get to actual latvia sea pressure
-    Serial.println(" m");
-
-    Serial.println();
+void transmitter(String outgoing, int messageId) {
+  LoRa.beginPacket();
+  LoRa.write(_destination);
+  LoRa.write(_localAddress);
+  LoRa.write(messageId);
+  LoRa.write(outgoing.length());
+  LoRa.print(outgoing);
+  LoRa.endPacket();
+  Serial.println("Package sent");
 }
-void loop() {
-  // try to parse packet
-  //int packetSize = LoRa.parsePacket();
-  //if (packetSize) {
-    // received a packet
-    //Serial.print("Received packet '");
-
-    // read packet
-   // while (LoRa.available()) {
-      //Serial.print((char)LoRa.read());
-    //}
-
-    // print RSSI of packet
-    //Serial.print("' with RSSI ");
-    //Serial.println(LoRa.packetRssi());
-    Barometer();
-    delay(20000);
-  }
+void loop(){
+    Serial.println("Sending barometer data");
+    float Temperature = bmp.readTemperature();
+    float pressure = bmp.readPressure();
+    float altitude = bmp.readAltitude(1017);
+    String Message = "Temperature: " + String(Temperature) + "*C; Pressure: " + String(pressure) + "Pa; Altitude: " + String(altitude); 
+   transmitter(Message, 0);
+   delay(10000);
+}
